fe7f981fa20eefc4aca0f4630878e1435b205640
Vitalii <vitalya.boss82@gmail.com>
2022-02-22 22:11:01 +0200

    home work 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+    'Potatoes',
+    'Mushrooms',
+    'Garlic',
+    'Tomatos',
+    'Herbs',
+    'Condiments',
 ];
+
+const listIngredients = document.querySelector("#ingredients");
+for (let i = 0; i < ingredients.length; i++) {
+    let liIngredient = document.createElement("li");
+    liIngredient.classList.add("item");
+    liIngredient.textContent = ingredients[i];
+    listIngredients.append(liIngredient);
+}
+console.log(listIngredients);
