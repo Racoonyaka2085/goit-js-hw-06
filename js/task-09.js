121df2f3c456f0b3a00b3102f8c86768d7cbcbf9
Vitalii <vitalya.boss82@gmail.com>
2022-03-31 20:11:06 +0300

    do the home work number 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,10 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const changeColor = document.querySelector(".change-color");
+const widgetColor = document.querySelector(".widget");
+const spanColor = document.querySelector(".color");
+changeColor.addEventListener("click", (e) => {
+    let randomColor = `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+    widgetColor.style.backgroundColor = randomColor;
+    spanColor.textContent = `-${randomColor}`;
+})
