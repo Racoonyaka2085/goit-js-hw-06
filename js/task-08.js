80f82eb212ecadecc0419276c8372d45f5acc37c
Vitalii <vitalya.boss82@gmail.com>
2022-03-31 12:56:21 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
-
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (form.elements.password.value === "" || form.elements.email.value === "") {
+        alert("Please fill in all the fields!");
+    }
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    console.log(`Login: ${email.value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+});
